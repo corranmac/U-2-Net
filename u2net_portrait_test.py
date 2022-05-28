c4a6924495d11145880465a7e3814527ecb08bc4
corranmac <corranmac@hotmail.co.uk>
2022-05-28 19:31:23 +0100

    Update u2net_portrait_test.py

--- u2net_portrait_test.py
+++ u2net_portrait_test.py
@@ -51,7 +51,7 @@ def save_output(image_name,pred,d_dir):
 
     imo.save(d_dir+'/'+imidx+'.png')
 
-def main():
+def main(image_dir,prediction_dir,batch_size,workers):
 
     # --------- 1. get image path and name ---------
     model_name='u2net_portrait'#u2netp
@@ -75,9 +75,9 @@ def main():
                                                                       ToTensorLab(flag=0)])
                                         )
     test_salobj_dataloader = DataLoader(test_salobj_dataset,
-                                        batch_size=1,
+                                        batch_size=int(batch_size),
                                         shuffle=False,
-                                        num_workers=1)
+                                        num_workers=int(workers))
 
     # --------- 3. model define ---------
 
@@ -114,4 +114,15 @@ def main():
         del d1,d2,d3,d4,d5,d6,d7
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description='U2Net')
+    parser.add_argument('--image_dir', metavar='path', required=True,
+                        help='the folder of your input frames')
+    parser.add_argument('--prediction_dir', metavar='path', required=True,
+                    help='the folder of result frames')
+    parser.add_argument('--batch_size', metavar='path', required=False,
+                  help='the batch size')
+    parser.add_argument('--workers', metavar='path', required=True,
+              help='the number of workers')
+    args = parser.parse_args()
+    main(image_dir=args.image_dir, prediction_dir=args.prediction_dir,batch_size=args.batch_size,workers=args.workers)
